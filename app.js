b66ea090ec740944d1bff7fbcf3f49dc772ea8cd
Sanam_App <ugyendechen00@gmail.com>
2023-06-08 09:16:35 +0600

    xv

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3003;
-const middleware = require("./middleware")
-const path = require('path')
+const middleware = require("./middleware");
+const path = require("path");
 const bodyParser = require("body-parser");
-const mongoose = require('./database');
+const mongoose = require("./database");
 const session = require("express-session");
 
-
-const server = app.listen(port, ()=> console.log("server is listening on port " + port));
-
+const server = app.listen(port, () =>
+  console.log("server is listening on port " + port)
+);
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
+app.use(
+  session({
     secret: "ugyen tech",
     resave: true,
-    saveUninitialized: false
-}))
-
+    saveUninitialized: false,
+  })
+);
 
 //routes
 const loginRoute = require("./routes/loginRoutes");
@@ -30,11 +31,8 @@ const registerRoute = require("./routes/registerRoutes");
 const logoutRoute = require("./routes/logout");
 const postRoute = require("./routes/postRoutes");
 
-
-
 //api routes
 const postsApiRoute = require("./routes/api/posts");
-
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
@@ -44,15 +42,12 @@ app.use("/posts", middleware.requireLogin, postRoute);
 //api
 app.use("/api/posts", postsApiRoute);
 
-
 app.get("/", middleware.requireLogin, (req, res, next) => {
+  var payload = {
+    pageTitle: "G-chat",
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+  };
 
-    var payload = {
-        pageTitle: 'Loju',
-        userLoggedIn: req.session.user,
-        userLoggedInJs: JSON.stringify(req.session.user),
-    }
-
-    res.status(200).render('home', payload);
-
-})
+  res.status(200).render("home", payload);
+});
